4bbbfa1028c4d6505e2a636d540c990b20b48efe
Numenorean <41187266+Numenorean@users.noreply.github.com>
2020-11-15 17:30:09 +0200

    Update keypresses.go

--- keypresses.go
+++ keypresses.go
@@ -1,25 +1,25 @@
-package keypresses
+package main
 
 import (
 	"syscall"
 	"unsafe"
+
 	"golang.org/x/sys/windows"
 )
 
 var (
-	mod 					= windows.NewLazyDLL("user32.dll")
-	procGetClassNameW 		= mod.NewProc("GetClassNameW")
-	procGetAsyncKeyState 		= mod.NewProc("GetAsyncKeyState")
-	classtolook = []string{
+	mod                  = windows.NewLazyDLL("user32.dll")
+	procGetClassNameW    = mod.NewProc("GetClassNameW")
+	procGetAsyncKeyState = mod.NewProc("GetAsyncKeyState")
+	classtolook          = []string{
 		"ConsoleWindowClass",
 	}
 )
 
 type (
 	HANDLE uintptr
-	HWND HANDLE
+	HWND   HANDLE
 )
-
 
 func getClassName(hwnd HWND) (name string, err error) {
 	n := make([]uint16, 256)
@@ -44,16 +44,16 @@ func getWindow(funcName string) uintptr {
 }
 
 func checkInArray(a string, list []string) bool {
-    for _, b := range list {
-        if b == a {
-            return true
-        }
-    }
-    return false
+	for _, b := range list {
+		if b == a {
+			return true
+		}
+	}
+	return false
 }
 
 /*
-Getting key state only if current window is active.
+With IsKeyPressedGlobal you can get key state only if current window is active.
 keyVirtualCode - win32api virtual code, you can get more info about it on microsoft win api documentation.
 "false" argument means that to get key state, window should be active.
 "true" argument means that to get key state, window might not be active, the same as an IsKeyPressed function.
@@ -63,15 +63,14 @@ func IsKeyPressedGlobal(keyVirtualCode int, global bool) bool {
 		return IsKeyPressed(keyVirtualCode)
 	}
 	if hwnd := getWindow("GetForegroundWindow"); hwnd != 0 {
-			cn , _ := getClassName(HWND(hwnd))
-			if checkInArray(cn ,classtolook) {
-				return IsKeyPressed(keyVirtualCode)
-			return false
+		cn, _ := getClassName(HWND(hwnd))
+		if checkInArray(cn, classtolook) {
+			return IsKeyPressed(keyVirtualCode)
 		}
+		return false
 	}
 	return false
 }
-
 
 /*
 Getting key state (even if window inactive).
@@ -80,7 +79,7 @@ keyVirtualCode - win32api virtual code, you can more info about it on microsoft 
 func IsKeyPressed(keyVirtualCode int) bool {
 	// Query key mapped to integer `0x00` to `0xFF` if it's pressed.
 	asynch, _, _ := procGetAsyncKeyState.Call(uintptr(keyVirtualCode))
-	
+
 	// If the least significant bit is set ignore it.
 	//
 	// As it's written in the documentation:
@@ -89,6 +88,6 @@ func IsKeyPressed(keyVirtualCode int) bool {
 	if asynch&0x1 == 0 {
 		return false
 	}
-	
+
 	return true
 }
